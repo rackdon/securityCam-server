a3d04cdc42e2aa96d682b2700faca742c451fe76
rackdon <martinez.calvo.ricardo@gmail.com>
2016-03-25 00:19:32 +0100

    installation script modified for mongo connections

--- conf/installation.sh
+++ conf/installation.sh
@@ -1,6 +1,11 @@
 sudo apt-get update
+sudo apt-get -y install nodejs-dev
 sudo apt-get -y install motion
-sudo apt-get -y install mongodb
+sudo apt-get -y install mongodb-server
+sudo mkdir -p /data/db
+sudo chmod 0755 /data/db
+sudo chown -R $USER /data/db
 sudo cp -f ./motion.conf /etc/motion/motion.conf
 sudo mkdir /home/motion/
 sudo chmod 777 /home/motion/
+sudo apt-get -y autoremove
