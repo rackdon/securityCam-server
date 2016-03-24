e93d3106cd42fca7fd9d4dd150f33b79168810b0
rackdon <martinez.calvo.ricardo@@gmail.com>
2016-03-24 09:40:52 +0100

    auto-confirmation while install packages added

--- conf/installation.sh
+++ conf/installation.sh
@@ -1,8 +1,6 @@
 sudo apt-get update
-sudo apt-get install nodejs-legacy
-sudo apt-get install npm
-sudo apt-get install motion
-sudo apt-get install mongodb
+sudo apt-get -y install motion
+sudo apt-get -y install mongodb
 sudo cp -f ./motion.conf /etc/motion/motion.conf
 sudo mkdir /home/motion/
 sudo chmod 777 /home/motion/
