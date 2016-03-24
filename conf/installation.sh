b6d79a2d6de60ad13f090ae75ae5b672a810ec42
rackdon <martinez.calvo.ricardo@@gmail.com>
2016-03-24 09:30:27 +0100

    added mongodb in the installation script

--- conf/installation.sh
+++ conf/installation.sh
@@ -1,8 +1,8 @@
-#sudo apt-get update
-#sudo apt-get install nodejs-legacy
-#sudo apt-get install npm
-#sudo apt-get install motion
-#sudo cp -f ./motion.conf /etc/motion/motion.conf
-#sudo mkdir /home/motion/
-#sudo chmod 777 /home/motion/
-echo "funciona"
+sudo apt-get update
+sudo apt-get install nodejs-legacy
+sudo apt-get install npm
+sudo apt-get install motion
+sudo apt-get install mongodb
+sudo cp -f ./motion.conf /etc/motion/motion.conf
+sudo mkdir /home/motion/
+sudo chmod 777 /home/motion/
