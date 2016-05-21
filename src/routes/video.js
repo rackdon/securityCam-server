53dd083a334a2310af871b672675fb4e44574f6e
rackdon <martinez.calvo.ricardo@gmail.com>
2016-05-21 20:48:26 +0200

    video route added

---
+++ src/routes/video.js
@@ -0,0 +1,8 @@
+var request = require('request')
+
+module.exports = function setup (app) {
+
+  app.get('/video/motion' function (req, res) {
+    request('http://localhost:8081').pipe(res)
+  })
+}
