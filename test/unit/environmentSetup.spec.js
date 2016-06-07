214f9a3fefad241ea5812f05e5f81daad589556b
rackdon <martinez.calvo.ricardo@gmail.com>
2016-06-07 20:39:50 +0200

    test fixed

--- test/unit/environmentSetup.spec.js
+++ test/unit/environmentSetup.spec.js
@@ -4,7 +4,7 @@ describe('In environments setup.js', function () {
   it('configuration variables are returned in production environment', function () {
     var configuration = environmentSetup('production')
 
-    expect(configuration).to.have.property('watchFolder', 'home/pi/motion/captures/')
+    expect(configuration).to.have.property('watchFolder', '/home/motion/pictures/')
     expect(configuration).to.have.property('db', 'mongodb://localhost/pictures')
   })
 
