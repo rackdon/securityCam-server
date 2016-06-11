bf0423662bb749b7a138731623d3067c3a00506e
rackdon <martinez.calvo.ricardo@gmail.com>
2016-06-11 12:43:17 +0200

    installation script improved

--- config/installation.sh
+++ config/installation.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 sudo apt-get update
-sudo apt-get -y install motion
-sudo apt-get -y install mongodb-server
+if $USER -ne "motion"; then
+  sudo mkdir /home/motion/
+  sudo chmod 777 /home/motion/
+fi
 sudo mkdir -p /data/db
 sudo chmod 0755 /data/db
 sudo chown -R $USER /data/db
+sudo apt-get -y install motion
+sudo apt-get -y install mongodb-server
 sudo cp -f ./config/motion.conf /etc/motion/motion.conf
-sudo mkdir /home/motion/
-sudo chmod 777 /home/motion/
 sudo apt-get -y autoremove
 sudo npm install -g grunt
+mkdir ./test/testFiles
 npm install
-mongod --shutdown
